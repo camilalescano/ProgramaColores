1de19d8a58638be36d00d9d1557d2ffc851f5317
Camila Lescano Vásquez <89618093+camilalescano@users.noreply.github.com>
2022-05-12 18:51:06 -0500

    He añadido los archivos del programa
    He añadido todos los archivos para que el programa funcione. También un README actualizado.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ProgramaColores
-Programa básico utilizando JS para obtener colores cuando se pasa el cursor. 
+##Descripción
+
+Código de práctica que realicé al aprender algunas bases de programación. Simplemente al pasar el cursor por los colores, la palabra central va cambiando su color. Se enfoca alrededor de una función principal en JS, y también para practicar lo que es el flexbox, junto con criterios básicos de diseño.
+
+##Autor
+Camila Lescano Vásquez
+<!--
+-Portafolio (Behance, Dribble)
+-Linkedin
+-->
+
+##Ejemplo en vivo
+*Por actualizar
+
+##Instalación
+Este proyecto no requiere instalación alguna, simplemente se abre el archivo index.html 
+
+##Contacto
+Puedes escribirme a camilalescano.lv@gmail.com para contactarme.
+
+##Uso
+Este código es libre de uso alguno. 
---
+++ recursos.js
@@ -0,0 +1,23 @@
+﻿function escribe(num) { 
+    var colores = ["ninguno", "rojo", "amarillo", "verde", "azul",
+    "morado","naranja","celeste","rosado","limón","gris"];
+    var texto = document.getElementById('respuesta');
+    texto.innerHTML = colores[num];
+    
+    switch(num){
+        case 1: texto.style.color = "#E74C3C"; break;
+        case 2: texto.style.color = "#ffd800"; break;
+        case 3: texto.style.color = "#27AE60"; break;
+        case 4: texto.style.color = "#1B4F72"; break;
+        case 5: texto.style.color = "#6C3483"; break;
+        case 6: texto.style.color = "orange"; break;
+        case 7: texto.style.color = "#3498DB"; break;
+        case 8: texto.style.color = "#F48FB1"; break;
+        case 9: texto.style.color = "#CDDC39"; break;
+        case 10: texto.style.color = "gray"; break;
+        default: texto.style.color = "black";
+    }
+    
+   
+}
+
